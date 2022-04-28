ac7af569a0d4032e86998c0a1381858998d10d91
AlexisSigcha97t <alexgabriel12000@gmail.com>
2022-04-27 23:52:35 -0500

    Agregar archivos de proyecto.

---
+++ deber2AlexisSigcha/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace deber2AlexisSigcha
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+            if ((user == "Gabriel") && (pass == "Gabo123"))
+            {
+                await Navigation.PushAsync(new vista_dos(user));
+
+            }
+            else
+            {
+                await DisplayAlert("Alerta", "Credenciales Incorectas", "cerrar");
+            }
+        }
+    }
+}
---
+++ deber2AlexisSigcha/vista_dos.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace deber2AlexisSigcha
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class vista_dos : ContentPage
+    {
+        public vista_dos(string user)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "Usuario activo: " + user;
+        }
+
+        private void btnResultado_Clicked(object sender, EventArgs e)
+        {
+            double ex1 = Convert.ToDouble(txtEx1.Text);
+            double ex2 = Convert.ToDouble(txtEx2.Text);
+            double seg1 = Convert.ToDouble(txtSeg1.Text);
+            double seg2 = Convert.ToDouble(txtSeg2.Text);
+
+            double par1 = (ex1 * 0.2) + (seg1 * 0.3);
+            lblResultado1.Text = par1.ToString();
+            double par2 = (ex2 * 0.2) + (seg2 * 0.3);
+            lblResultado2.Text = par2.ToString();
+            double final = par1 + par2;
+            lblFinal.Text = final.ToString();
+            if (final >= 7)
+            {
+                lblFinalRes.Text = "Aprovado";
+            }
+
+            if (final >= 5 && final <= 6.9)
+            {
+                lblFinalRes.Text = "Complementario";
+            }
+
+            if (final < 5)
+            {
+                lblFinalRes.Text = "Reprovado";
+            }
+
+        }
+    }
+}
